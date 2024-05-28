c77f5f5458c9bd6b4dcf2c5c987bdb77ae78dc08
hassanshakil22 <hassanshakil.workspace@gmail.com>
2024-05-28 20:07:06 +0500

    completed allhamdulillah

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/models/weather_model.dart';
 import 'package:weatherapp/screens/bigcard_view.dart';
 // import 'package:intl/intl.dart';
 // import 'package:weatherapp/models/weather_model.dart';
 import 'package:weatherapp/screens/card_view.dart';
+import 'package:weatherapp/service/api_service.dart';
+import 'package:weatherapp/utils/colors.dart';
 // import 'package:weatherapp/service/api_service.dart';
 // import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 // import 'package:weatherapp/utils/colors.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   final String cityname;
 
   const HomeView({super.key, required this.cityname});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  WeatherModel? weather;
+  bool? isDaytime;
+
+  Future _fetchweatherData() async {
+    final response = await ApiService.getWeatherApi(widget.cityname);
+    setState(() {
+      weather = response;
+    });
+  }
+
+  bool isDay({required int? dt, required int? sunrise, required int? sunset}) {
+    if (dt != null && sunrise != null && sunset != null) {
+      return isDaytime = dt >= sunrise && dt < sunset;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchweatherData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-        ),
+        backgroundColor: isDay(
+                dt: weather?.dt,
+                sunrise: weather?.sys?.sunrise,
+                sunset: weather?.sys?.sunset)
+            ? AppColors.appBackgroundDay
+            : AppColors.appBackgroundNight,
         body: Center(
-          child: Row(
-            children: [
-              CardView(
-                cityname: cityname,
-              ),
-              BigcardView(cityname: cityname),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 3),
+            child: Row(
+              children: [
+                CardView(
+                  cityname: widget.cityname,
+                ),
+                BigcardView(cityname: widget.cityname),
+              ],
+            ),
           ),
         ));
   }
 }
-
-TextEditingController _controller = TextEditingController();
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static Color appBackgroundDay = const Color(0xFFe3e8e4);
+  static Color appBackgroundNight = const Color(0xFF808080);
   static Color cardfontColor = const Color(0xFF000000);
   static Color bigcardfontColor = const Color(0xFFFFFFFF);
 
